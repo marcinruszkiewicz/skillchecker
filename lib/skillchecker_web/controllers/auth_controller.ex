a4e9b07c9d38b926f2e417d4f1047ba62c5ac6d7
Marcin Ruszkiewicz <marcin.ruszkiewicz@aevi.pl>
2024-10-05 20:40:15 +0200

    redirect to character if session already logged in with oauth

--- lib/skillchecker_web/controllers/auth_controller.ex
+++ lib/skillchecker_web/controllers/auth_controller.ex
@@ -7,30 +7,42 @@ defmodule SkillcheckerWeb.AuthController do
   alias Skillchecker.Characters.CharacterSkills
 
   def callback(conn, _params) do
-    attrs = %{
-      eveid: conn.assigns.ueberauth_auth.extra.raw_info.user.character_id,
-      name: conn.assigns.ueberauth_auth.extra.raw_info.user.name,
-      owner_hash: conn.assigns.ueberauth_auth.extra.raw_info.user.owner_hash,
-      token: conn.assigns.ueberauth_auth.credentials.token,
-      refresh_token: conn.assigns.ueberauth_auth.credentials.refresh_token,
-      expires_at: conn.assigns.ueberauth_auth.credentials.expires_at |> DateTime.from_unix!(:second),
-      accepted: false
-    }
+    if owner_hash = get_session(conn, :owner_hash) do
+      character = Skillchecker.Characters.get_character_by_owner_hash(owner_hash)
 
-    character = Skillchecker.Characters.add_character(attrs)
+      redirect_to_character(conn, character)
+    else
+      attrs = %{
+        eveid: conn.assigns.ueberauth_auth.extra.raw_info.user.character_id,
+        name: conn.assigns.ueberauth_auth.extra.raw_info.user.name,
+        owner_hash: conn.assigns.ueberauth_auth.extra.raw_info.user.owner_hash,
+        token: conn.assigns.ueberauth_auth.credentials.token,
+        refresh_token: conn.assigns.ueberauth_auth.credentials.refresh_token,
+        expires_at: conn.assigns.ueberauth_auth.credentials.expires_at |> DateTime.from_unix!(:second),
+        accepted: false
+      }
 
-    character
-    |> CharacterData.update_from_esi(conn.assigns.ueberauth_auth.credentials.token)
-    |> CharacterSkills.update_from_esi(conn.assigns.ueberauth_auth.credentials.token)
+      character = Skillchecker.Characters.add_character(attrs)
 
+      character
+      |> CharacterData.update_from_esi(conn.assigns.ueberauth_auth.credentials.token)
+      |> CharacterSkills.update_from_esi(conn.assigns.ueberauth_auth.credentials.token)
+
+      conn =
+        conn
+        |> put_session(:owner_hash, conn.assigns.ueberauth_auth.extra.raw_info.user.owner_hash)
+
+      redirect_to_character(conn, character)
+    end
+  end
+
+  defp redirect_to_character(conn, character) do
     case character.accepted do
       false ->
         conn
-        |> put_session(:owner_hash, conn.assigns.ueberauth_auth.extra.raw_info.user.owner_hash)
         |> redirect(to: ~p"/waiting/#{character}")
       true ->
         conn
-        |> put_session(:owner_hash, conn.assigns.ueberauth_auth.extra.raw_info.user.owner_hash)
         |> redirect(to: ~p"/characters/#{character}")
     end
   end
--- lib/skillchecker_web/controllers/page_controller.ex
+++ lib/skillchecker_web/controllers/page_controller.ex
@@ -2,7 +2,13 @@ defmodule SkillcheckerWeb.PageController do
   use SkillcheckerWeb, :controller
 
   def user_landing(conn, _params) do
-    render conn, :user_landing, layout: false
+    if owner_hash = get_session(conn, :owner_hash) do
+      character = Skillchecker.Characters.get_character_by_owner_hash(owner_hash)
+
+      redirect_to_character(conn, character)
+    else
+      render conn, :user_landing, layout: false
+    end
   end
 
   def admin_landing(conn, _params) do
@@ -15,5 +21,16 @@ defmodule SkillcheckerWeb.PageController do
 
   def user_waiting(conn, %{"id" => id} = _params) do
     render conn, :user_waiting, layout: false, id: id
+  end
+
+  defp redirect_to_character(conn, character) do
+    case character.accepted do
+      false ->
+        conn
+        |> redirect(to: ~p"/waiting/#{character}")
+      true ->
+        conn
+        |> redirect(to: ~p"/characters/#{character}")
+    end
   end
 end
